8d212a6d7b6f05531a0089c986b0134a20cae12e
akshanshverma <akshansh.verma@outlook.com>
2018-10-08 19:19:07 +0530

    8/10/2018

--- src/Util.java
+++ src/Util.java
@@ -1,24 +1,57 @@
 import java.util.ArrayList;
 
+/******************************************************************************
+ * Purpose: Helper class containing static method.
+ *
+ * @author Akshansh Verma
+ * @version 1.0
+ * @since 06-08-2018
+ *
+ ******************************************************************************/
 public class Util {
+	/**
+	 * Function to check two string are anagram or not
+	 * 
+	 * @param a string value of 1st string
+	 * @param b string value of 2nd string
+	 * @return true or false if both stings are anagram then true otherwise false
+	 */
 	public static boolean anagram(String a, String b) {
+		/*
+		 * Anagram is possible only when the size of string is same so if both string
+		 * full fill the condition of if then it will enter inside the if otherwise
+		 * return false
+		 */
 		if (a.length() == b.length()) {
+
 			char[] ar = a.toCharArray();
 			char[] br = b.toCharArray();
 			ArrayList<Character> al = new ArrayList<Character>();
 			ArrayList<Character> bl = new ArrayList<Character>();
-
+			/*
+			 * for loop to store the value of char array to Character type ArrayList al 1st
+			 * ArrayList bl 2nd ArrayList
+			 */
 			for (char c : ar) {
 				al.add(c);
 			}
 			for (char ch : br) {
 				bl.add(ch);
 			}
+			/*
+			 * forEach loop to retrieve the value of bl ArrayList one by one and if
+			 * condition to check that al contain value of bl which store in character if al
+			 * contain then remove method will remove it for al
+			 */
 			for (Character character : bl) {
 				if (al.contains(character)) {
 					al.remove(character);
 				}
 			}
+			/*
+			 * after forEach loop if size of al is 0 then all character of both string are
+			 * same
+			 */
 			if (al.isEmpty()) {
 				return true;
 			}
@@ -26,13 +59,34 @@ public class Util {
 		return false;
 	}
 
+	/**
+	 * Function is use to print integer anagram of prime number
+	 * 
+	 * @param alMain ArrayList type input it take prime number list
+	 * 
+	 *               then print all prime number which are anagram
+	 */
 	public static void anagramInt(ArrayList<Integer> alMain) {
-
+		/*
+		 * al1 ArrayList to store the value of number as single single digit al2
+		 * ArrayList to store the value of other number as single single digit
+		 * 
+		 * temp1 store single digit of number and save it in the ArrayList al1 temp2
+		 * store single digit of other number and save it in other ArrayList al2
+		 */
 		ArrayList<Integer> al1 = new ArrayList<Integer>();
 		ArrayList<Integer> al2 = new ArrayList<Integer>();
 
 		int temp1, temp2;
+		/*
+		 * two for loop one for hold one value other is for retrieve all next value to
+		 * compare the hold value
+		 */
 		for (int i = 0; i < alMain.size() - 1; i++) {
+			/*
+			 * n1 hold the number alMain ArrayList while loop store digit or number in
+			 * ArrayList al1 one by one
+			 */
 			int n1 = alMain.get(i);
 			while (n1 > 0) {
 				temp1 = n1 % 10;
@@ -40,12 +94,20 @@ public class Util {
 				n1 = n1 / 10;
 			}
 			for (int j = i + 1; j < alMain.size(); j++) {
+				/*
+				 * n2 retrieve next value for compare and store in al2 ArrayList
+				 */
 				int n2 = alMain.get(j);
 				while (n2 > 0) {
 					temp2 = n2 % 10;
 					al2.add(temp2);
 					n2 = n2 / 10;
 				}
+				/*
+				 * forEach loop to retrieve the value of al1 ArrayList one by one and if
+				 * condition to check that al2 contain value of al1 which store in character if
+				 * al2 contain then remove method will remove it for al2
+				 */
 				for (Integer it : al1) {
 					if (al2.contains(it)) {
 						al2.remove(it);
@@ -54,15 +116,32 @@ public class Util {
 				if (al2.isEmpty()) {
 					System.out.println(alMain.get(i) + " " + alMain.get(j));
 				}
+				/*
+				 * removeAll method to make ArrayList empty so the it can store next value after
+				 * compare
+				 */
 				al2.removeAll(al2);
 			}
 			al1.removeAll(al1);
 		}
 	}
 
+	/**
+	 * function to check number is palindrome or not
+	 * 
+	 * @param n int to pass number which we want to check
+	 * @return true or false that number is palindrome or not
+	 */
 	public static boolean palindrome(int n) {
-
+		/*
+		 * for palindrome we need minimum two digit number so if number is greater then
+		 * 9 then only possible it is palindrome
+		 */
 		if (n > 9) {
+			/*
+			 * ArrayList al is to save number one by one digit while loop to retrieve digit
+			 * from number one by one
+			 */
 			ArrayList<Integer> al = new ArrayList<Integer>();
 			int temp;
 			while (n > 0) {
@@ -70,7 +149,11 @@ public class Util {
 				al.add(temp);
 				n = n / 10;
 			}
-
+			/*
+			 * temp2 last index of ArrayList for loop to check value form 0 index and temp2
+			 * to check value value from last index if at any point value is unequal then
+			 * return false otherwise true
+			 */
 			int temp2 = al.size() - 1;
 			for (int i = 0; i < al.size() / 2; i++) {
 
@@ -85,9 +168,22 @@ public class Util {
 		return false;
 	}
 
+	/**
+	 * function to print prime number 0 to 1000
+	 * 
+	 * @return ArrayList of prime number
+	 */
 	public static ArrayList<Integer> primeNumbers() {
 		ArrayList<Integer> al = new ArrayList<Integer>();
+		/*
+		 * for loop initialize from 2 because 0 and 1 are not prime number
+		 */
 		for (int i = 2; i < 1000; i++) {
+			/*
+			 * for loop to check that number is divisible by any other number or not if it
+			 * is divisible by any other number then loop will break and return false
+			 */
+
 			boolean b = true;
 			for (int j = 2; j <= i / 2; j++) {
 				if (i % j == 0) {
@@ -95,16 +191,31 @@ public class Util {
 					break;
 				}
 			}
+			/*
+			 * if number is divisible by only one and itself
+			 * the it will add to ArrayList
+			 */
 			if (b == true) {
 				al.add(i);
 			}
 		}
 		return al;
 	}
-
+	
+	/**
+	 * function to perform binary search on array
+	 * @param n int is a number which we want to search in array
+	 * @param arr int type array in which we perform search operation
+	 * @return int value which is index of the number in array list.
+	 * if number is not present in Array the it will return -1
+	 */
 	public static int binarySearch(int n, int[] arr) {
+		/*
+		 * insertionSort to sort the array because binary search can perform only
+		 * on sorted array 
+		 */
 		int[] ar = insertionSort(arr);
-
+		
 		int low = 0;
 		int hig = ar.length - 1;
 		int mid = 0;
